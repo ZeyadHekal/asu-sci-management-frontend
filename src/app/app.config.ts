14e04bcad948eedf894f4d36e999a92c3aef22a2
Basil Abo-Al-Nasr <zain.ahmedioq@gmail.com>
2024-12-10 00:56:00 +0200

    Login

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,18 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { environment } from '../environments/enviroment';
 import { routes } from './app.routes';
+import {
+  provideHttpClient,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
+import { BASE_PATH } from './api';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideHttpClient(withInterceptorsFromDi()),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    { provide: BASE_PATH, useValue: environment.apiUrl },
+  ],
 };
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,53 +1,61 @@
 import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormGroup , FormControl ,ReactiveFormsModule , Validators} from '@angular/forms';
-import { Router} from '@angular/router';
-import { AuthService } from '../../core/services/auth.service';
+import {
+  FormGroup,
+  FormControl,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../../api/api/auth.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ReactiveFormsModule,NgIf],
+  imports: [ReactiveFormsModule, NgIf],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
   loginForm = new FormGroup({
     username: new FormControl('', [Validators.required]),
-    password: new FormControl('', [Validators.required])
+    password: new FormControl('', [Validators.required]),
   });
-
   isloading = false;
-
-  constructor(private auth: AuthService, private router: Router) {}
-
   ngOnInit(): void {
     setTimeout(() => {
       this.isloading = false;
     }, 3000);
   }
 
-  login(userName:string,password:string) {
-    const credentials = { email: userName, password: password };
-    this.auth.login(credentials).subscribe(
-      (response) => {
-        if (response) {
-          console.log('Login successful:', response);
-          this.router.navigate(['/landing']);
-        } else {
-          console.error('Login failed');
-        }
+  constructor(private authService: AuthService,private router: Router) {}
+  login(username: string, password: string) {
+    const loginRequestDto = {
+      username,
+      password,
+    };
+
+    this.authService.authControllerLogin(loginRequestDto, 'body').subscribe({
+      next: (response) => {
+        // نجاح تسجيل الدخول
+        console.log('Login Successful:', response);
+        localStorage.setItem('accessToken', response.accessToken);
+        localStorage.setItem('refreshToken', response.refreshToken);
       },
-      (error) => {
-        console.error('Login error:', error);
-      }
-    );
+      error: (error) => {
+        // فشل تسجيل الدخول
+        this.router.navigate(['/landing']);
+        console.error('Login Failed:', error);
+      },
+    });
   }
 
   onSubmit() {
     if (this.loginForm.valid) {
-      this.login('johndoe@example.com','password123');
+      this.login(
+        this.loginForm.get("username")!.value!,
+        this.loginForm.get('password')!.value!
+      );
     }
   }
-  
 }
